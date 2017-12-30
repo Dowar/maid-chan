42a9cc2dbd9228e5676da3bf87c3f36b3c59a9af
Dowar <dowarlordof@hotmail.fr>
2017-12-30 06:36:03 +0100

    V0.0.3

--- IA/dialogues.js
+++ IA/dialogues.js
@@ -77,25 +77,83 @@ exports.run = (client , fs, message, mentionned, data, dialogue, id, tag) =>
         if (message.author.id !== id)
         {
             var r = Math.floor((Math.random()*10))
-            if(r<=4)
+            if(r>=0 && r<=0)
             {
                 message.channel.send("Joyeux Noël " + message.author.username + " !",
                 {
-                    files: 
-                    [
-                      "./images/noel_1.png"
-                    ]
+                    "embed": 
+                    {
+                        "color": 10073378,
+                        "image": 
+                        {"url": "https://dncache-mauganscorp.netdna-ssl.com/thumbseg/1575/1575662-bigthumbnail.jpg"}
+                    }
                 })
+                return
             }
-            if(r>=5)
+            if(r>=1 && r<=2)
             {
                 message.channel.send("Joyeux Noël " + message.author.username + " !",
                 {
-                    files: 
-                    [
-                      "./images/noel_2.png"
-                    ]
+                    "embed": 
+                    {
+                        "color": 10073378,
+                        "image": 
+                        {"url": "http://www.wallpapermaiden.com/image/2017/01/10/re-zero-emilia-santa-clothes-smiling-white-hair-cute-anime-11876.jpg"}
+                    }
                 })
+                return
+            }
+            if(r>=3 && r<=4)
+            {
+                message.channel.send("Joyeux Noël " + message.author.username + " !",
+                {
+                    "embed": 
+                    {
+                        "color": 10073378,
+                        "image": 
+                        {"url": "https://i.pinimg.com/originals/c3/e1/00/c3e100447886edf3ab07ab7b1bcaab8d.jpg"}
+                    }
+                })
+                return
+            }
+            if(r>=5 && r<=6)
+            {
+                message.channel.send("Joyeux Noël " + message.author.username + " !",
+                {
+                    "embed": 
+                    {
+                        "color": 10073378,
+                        "image": 
+                        {"url": "https://i.pinimg.com/originals/1f/83/5b/1f835b49538368864f256fe2fa26ac5f.jpg"}
+                    }
+                })
+                return
+            }
+            if(r>=7 && r<=8)
+            {
+                message.channel.send("Joyeux Noël " + message.author.username + " !",
+                {
+                    "embed": 
+                    {
+                        "color": 10073378,
+                        "image": 
+                        {"url": "https://vignette.wikia.nocookie.net/steamtradingcards/images/3/3c/Sakura_Santa_Artwork_6.jpg/revision/latest?cb=20160123103843"}
+                    }
+                })
+                return
+            }
+            if(r>=9 && r<=10)
+            {
+                message.channel.send("Joyeux Noël " + message.author.username + " !",
+                {
+                    "embed": 
+                    {
+                        "color": 10073378,
+                        "image": 
+                        {"url": "https://tugaleres.files.wordpress.com/2014/11/kuroyukihime_santa_anime_accel-world.jpg"}
+                    }
+                })
+                return
             }
         }
     }
