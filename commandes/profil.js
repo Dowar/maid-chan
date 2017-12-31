2fa9a1e7f9981c9fca67f24981ab5fe1d92f10b3
Dowar <dowarlordof@hotmail.fr>
2017-12-31 15:19:45 +0100

    31/12/2017

--- commandes/profil.js
+++ commandes/profil.js
@@ -33,7 +33,7 @@ exports.run = (client,fs,message,args,mentionned,data,commande,id,tag,author) =>
             })
             await messageInteractif.react("💶")
             await messageInteractif.react("🤗")
-            const collecteur = messageInteractif.createReactionCollector((reaction, user) => user.id === user.id)
+            const collecteur = messageInteractif.createReactionCollector((reaction, user) => user.id === author.id)
             collecteur.on("collect", async(reaction) => 
             {
                 if (reaction.emoji.name === "💶")
